945831959f3a73c0f5ed1c8a9dd2c272c01d4b04
José Ignacio <josignaciot@gmail.com>
2020-09-28 17:47:44 -0300

    tratando de mejorar la funcionalidad

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ circle.setAttribute("stroke-dasharray", perimeter);
 let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart(totalDuration) {
-    duration = totalDuration; // Guardamos el tiempo total cuando el tiempo esta en pausa y se clickea el boton start
+    // Asignar solo al inicio
+    if (!duration) {
+      duration = totalDuration; // Guardamos el tiempo total cuando el tiempo esta en pausa y se clickea el boton start
+    }
   },
   onTick(timeRemaining) {
     // Agrega el atributo a la etiqueta svg cada vez que se ejecuta tick y onTick
